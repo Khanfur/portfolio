33d03bdc102010e09e184250a7fd9f6c713e5c43
Khanfur <brandon.tromp@gmail.com>
2025-07-22 10:17:30 +0200

    add basic header

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+import Header from "./components/header";
 
 export const metadata: Metadata = {
   title: "Portfolio",
@@ -25,8 +16,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        
       >
+        <Header />
         {children}
       </body>
     </html>
